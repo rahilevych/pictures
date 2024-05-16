51fcd4f7c390b78548471968b5b7a9d49043ac77
AlonaRahilevych <ragilevic@gmail.com>
2024-05-16 09:30:15 +0200

    links into nav were added

--- src/components/NavComponent/NavComponent.tsx
+++ src/components/NavComponent/NavComponent.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './NavComponent.scss';
+import { Link } from 'react-router-dom';
 type NavComponentProps = {};
 
 const NavComponent = (props: NavComponentProps) => {
   return (
     <nav className='nav'>
-      <div className='container'>
+      <div className='container nav__container'>
         {' '}
         <p className='nav__logo'>LOGO</p>
-        <button className='nav__btn'>Registration</button>
+        <button className='nav__btn'>
+          {' '}
+          <Link to='/registration'>Registration</Link>
+        </button>
       </div>
     </nav>
   );
