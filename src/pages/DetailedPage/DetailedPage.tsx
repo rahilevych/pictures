ceb0923d88be661dc35d8dbe316f5bd832c0ca58
AlonaRahilevych <ragilevic@gmail.com>
2024-05-16 09:27:25 +0200

    some additional pages were added

---
+++ src/pages/DetailedPage/DetailedPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const DetailedPage = (props: Props) => {
+  return <div>DetailedPage</div>;
+};
+
+export default DetailedPage;
---
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const RegistrationPage = (props: Props) => {
+  return <div>RegistrationPage</div>;
+};
+
+export default RegistrationPage;
