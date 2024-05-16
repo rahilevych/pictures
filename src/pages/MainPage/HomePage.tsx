d4e709751ecdf61f8d21e55357e0486a859c99f8
AlonaRahilevych <ragilevic@gmail.com>
2024-05-16 09:28:52 +0200

    fetching of data was added

--- src/pages/MainPage/HomePage.tsx
+++ src/pages/MainPage/HomePage.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import HeaderComponent from '../../components/HeaderComponent/HeaderComponent';
+import React, { useEffect, useState } from 'react';
+
 import MainBlockComponent from '../../components/MainBlockComponent/MainBlockComponent';
 import NavComponent from '../../components/NavComponent/NavComponent';
 import Footer from '../../components/FooterComponent/Footer';
+import SearchBlock from '../../components/SearchBlock/SearchBlock';
+import axios from 'axios';
+import { ImageType } from '../../assets/types/ImageType';
+import { apiKey } from '../../config/APIKey';
 
-type HomePageProps = {};
+const HomePage = () => {
+  let [images, setImages] = useState<null | ImageType[]>(null);
+  let [tag, setTag] = useState<string>('blue');
+  let [photosPerPage, setPhotoPerPage] = useState<number>(50);
 
-const HomePage = (props: HomePageProps) => {
+  console.log(images);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=${apiKey}&q=${tag}&per_page=${photosPerPage}`
+      );
+      setImages(response.data.hits);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='wrapper'>
-      <NavComponent />
-      <HeaderComponent />
-      <MainBlockComponent />
-      <Footer />
+      <MainBlockComponent images={images} />
     </div>
   );
 };
