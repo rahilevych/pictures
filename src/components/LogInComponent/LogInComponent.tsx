691aaae848fafdca04a236a2422ae4f2c990e712
AlonaRahilevych <ragilevic@gmail.com>
2024-05-27 11:25:52 +0200

    validation was added on user side

--- src/components/LogInComponent/LogInComponent.tsx
+++ src/components/LogInComponent/LogInComponent.tsx
@@ -1,18 +1,79 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 
 import './LogInComponent.scss';
+import { AuthContext } from '../../context/AutorizationContext';
+import { auth } from '../../config/firebase';
 
 const LogInComponent = () => {
+  const { signIn, setPassword, setEmail, email, password } =
+    useContext(AuthContext);
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  const handleInputChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setEmail(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      email: validateEmail(value),
+    }));
+  };
+
+  const handleInputChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      setPassword(e.target.value);
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: validatePassword(e.target.value),
+      }));
+    }
+  };
+
+  const validateEmail = (email: string) => {
+    if (!email) {
+      return 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      return 'invalid  email';
+    }
+  };
+  const validatePassword = (password: string) => {
+    if (!password) {
+      return 'Password is required';
+    } else if (password.length < 6) {
+      return 'Password is too short';
+    }
+    return '';
+  };
+
+  console.log(auth.currentUser);
   return (
     <div className='login'>
-      <form action=''>
-        <label htmlFor='email-username'>Email/Username:</label>
-        <input type='text' id='email-username' name='email-username' />
-
-        <label htmlFor='password'>Password:</label>
-        <input type='password' id='password' name='password' />
-
-        <button type='submit'>Log In</button>
+      <form onSubmit={submitForm}>
+        <div className='login__input'>
+          <label htmlFor='email-username'>Email:</label>
+          <input
+            type='text'
+            id='email-username'
+            name='email-username'
+            onChange={handleInputChangeEmail}
+          />
+          {errors.email && <div className='error'>{errors.email}</div>}
+        </div>
+        <div className='login__input'>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            name='password'
+            onChange={handleInputChangePass}
+          />
+          {errors.password && <div className='error'>{errors.password}</div>}
+        </div>
+        <button onClick={signIn}>Log In</button>
       </form>
     </div>
   );
--- src/components/SignUpComponent/SignUpComponent.tsx
+++ src/components/SignUpComponent/SignUpComponent.tsx
@@ -1,20 +1,77 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './SignUpComponent.scss';
+import { auth } from '../../config/firebase.ts';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { AuthContext } from '../../context/AutorizationContext.tsx';
+import { set } from 'firebase/database';
 
 const SignUpComponent = () => {
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
+  const { register, setEmail, setPassword, user, email, password } =
+    useContext(AuthContext);
+
+  const handleInputChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setEmail(value);
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      email: validateEmail(value),
+    }));
+  };
+
+  const handleInputChangePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      setPassword(e.target.value);
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password: validatePassword(e.target.value),
+      }));
+    }
+  };
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const validateEmail = (email: string) => {
+    if (!email) {
+      return 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      return 'invalid  email';
+    }
+  };
+  const validatePassword = (password: string) => {
+    if (!password) {
+      return 'Password is required';
+    } else if (password.length < 6) {
+      return 'Password is too short';
+    }
+    return '';
+  };
+  console.log(user);
+  console.log(email);
+  console.log(password);
   return (
     <div className='signup'>
-      <form action=''>
-        <label htmlFor='username'>*User Name:</label>
-        <input type='text' id='username' name='username' />
-
-        <label htmlFor='email'>*Email:</label>
-        <input type='email' id='email' name='email' />
-
-        <label htmlFor='password'>*Password:</label>
-        <input type='password' id='password' name='password' />
-
-        <button type='submit'>Join</button>
+      <form onSubmit={submitForm}>
+        <label htmlFor='email'>Email:</label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          onChange={handleInputChangeEmail}
+        />
+        {errors.email && <div className='error'>{errors.email}</div>}
+        <label htmlFor='password'>Password:</label>
+        <input
+          type='password'
+          id='password'
+          name='password'
+          onChange={handleInputChangePass}
+        />
+        {errors.password && <div className='error'>{errors.password}</div>}
+        <button onClick={register}>Join</button>
       </form>
     </div>
   );
