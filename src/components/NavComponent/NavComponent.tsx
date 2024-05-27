642fae96203430428439a4cd1e0781db71357eb8
AlonaRahilevych <ragilevic@gmail.com>
2024-05-27 11:25:24 +0200

    login/out reaction was added in navbar component

--- src/components/NavComponent/NavComponent.tsx
+++ src/components/NavComponent/NavComponent.tsx
@@ -1,20 +1,49 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import './NavComponent.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import SearchComponent from '../SearchComponent/SearchComponent';
+import { AuthContext } from '../../context/AutorizationContext';
 type NavComponentProps = {
   input: React.ChangeEventHandler<HTMLInputElement>;
 };
 
 function NavComponent() {
+  const { isLoggedIn, logOut, setLoginPressed, setSignUpPressed } =
+    useContext(AuthContext);
+  console.log(isLoggedIn);
+
+  const handleSignUp = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setLoginPressed(false);
+    setSignUpPressed(true);
+  };
+  const handleLogin = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setLoginPressed(true);
+    setSignUpPressed(false);
+  };
+
   return (
     <nav className='nav'>
       <div className='container nav__container'>
-        <p className='nav__logo'>LOGO</p>
-
-        <Link to='/registration'>
-          <button className='nav__btn'>Log in</button>{' '}
-        </Link>
+        <NavLink to={'/'}>
+          <p className='nav__logo'>LOGO</p>
+        </NavLink>
+        {!isLoggedIn && (
+          <Link to='/registration'>
+            <div className='registration__links'>
+              <button className='nav__btn' onClick={handleLogin}>
+                Log in
+              </button>
+              <button className='nav__btn' onClick={handleSignUp}>
+                Sign up
+              </button>
+            </div>
+          </Link>
+        )}
+        {isLoggedIn && (
+          <button className='nav__btn' onClick={logOut}>
+            Log out
+          </button>
+        )}
       </div>
     </nav>
   );
