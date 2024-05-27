778f483fdff8d8384822dcd0b83b9ab9ad58c71e
AlonaRahilevych <ragilevic@gmail.com>
2024-05-27 11:26:20 +0200

    some styles were added to detailed page

--- src/pages/DetailedPage/DetailedPage.tsx
+++ src/pages/DetailedPage/DetailedPage.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
 import { ImageType } from '../../assets/types/ImageType';
+import './DetailedPage.scss';
 
 type DetailedPageProps = {
   images: ImageType[] | null;
@@ -11,28 +12,53 @@ const DetailedPage: FC<DetailedPageProps> = ({ images }) => {
   const img = images?.find((image) => image.id === Number(id));
 
   return (
-    <main className='details-block'>
-      <div className='details-block__descr'>
-        <div className='details-block__img'>
-          <img src={img?.largeImageURL} alt='' />
-        </div>
-        <div className='details-block__info'>
-          <div className='details-block__row'>
-            <p>Views</p>
-            <p>{img?.views}</p>
+    <div className='wrapper details__wrapper'>
+      <div className='container details__container'>
+        <main className='details'>
+          <div className='details__descr'>
+            <div className='details__img'>
+              <img src={img?.largeImageURL} alt='' />
+            </div>
+            <div className='details__info'>
+              <div className='details__row'>
+                <div className='details__icon'>
+                  <i className='ph ph-heart-straight'></i>
+                </div>
+                <div className='details__icon'>
+                  <i className='ph ph-bookmark-simple'></i>
+                </div>
+                <div className='details__icon'>
+                  <i className='ph ph-share-network'></i>
+                </div>
+              </div>
+              <div className='details__row'>
+                <p>Views</p>
+                <p>{img?.views}</p>
+              </div>
+              <div className='details__row'>
+                <p>Downloads</p>
+                <p>{img?.downloads}</p>
+              </div>
+              <div className='details__row'>
+                <p>Likes</p>
+                <p>{img?.likes}</p>
+              </div>
+              <div className='details__row'>
+                <p>Comments</p>
+                <p>{img?.comments}</p>
+              </div>
+              <div className='details__row'>
+                <div className='details__user-img'>
+                  <img src={img?.userImageURL} alt='' />
+                </div>
+                <p className='details__user'>{img?.user}</p>
+              </div>
+            </div>
           </div>
-          <div className='details-block__row'>
-            <p>Downloads</p>
-            <p>{img?.downloads}</p>
-          </div>
-          <div className='details-block__row'>
-            <p>Likes</p>
-            <p>{img?.likes}</p>
-          </div>
-        </div>
+          <div className='details__coments'></div>
+        </main>
       </div>
-      <div className='details-block__coments'></div>
-    </main>
+    </div>
   );
 };
 
