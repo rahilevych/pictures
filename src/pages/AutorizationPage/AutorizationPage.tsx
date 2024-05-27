24598113d173e1cb8207bb0b07270e9f39408832
AlonaRahilevych <ragilevic@gmail.com>
2024-05-27 11:27:26 +0200

    changes to autorization wwre added

--- src/pages/AutorizationPage/AutorizationPage.tsx
+++ src/pages/AutorizationPage/AutorizationPage.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './AutorizationPage.scss';
 import SignUpComponent from '../../components/SignUpComponent/SignUpComponent';
 import LogInComponent from '../../components/LogInComponent/LogInComponent';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../../context/AutorizationContext';
 
 const AutorizationPage = () => {
+  const { setLoginPressed, setSignUpPressed, signUpPressed, loginPressed } =
+    useContext(AuthContext);
+
+  const handleSignUp = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    setLoginPressed(false);
+    setSignUpPressed(true);
+  };
+  const handleLogin = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    setLoginPressed(true);
+    setSignUpPressed(false);
+  };
+
   return (
-    <div className='wrapper'>
+    <div className='wrapper autorization__wrapper'>
       <div className='autorization'>
         <div className='container autorization__container'>
           <div className='autorization__window'>
             <div className='autorization__buttons'>
-              <button className=' autorization__btn autorization__signup-btn '>
+              <a
+                onClick={handleSignUp}
+                className=' autorization__btn autorization__signup-btn '>
                 Sign up
-              </button>
-              <button className='autorization__btn autorization__login-btn'>
+              </a>
+              <a
+                onClick={handleLogin}
+                className='autorization__btn autorization__login-btn'>
                 Log in
-              </button>
+              </a>
             </div>
             <div className='autorization__form-block'>
-              <SignUpComponent />
-              <LogInComponent />
+              {(signUpPressed && <SignUpComponent />) ||
+                (loginPressed && <LogInComponent />)}
             </div>
           </div>
         </div>
