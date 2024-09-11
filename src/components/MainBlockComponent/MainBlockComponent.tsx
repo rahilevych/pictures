dcb9c5f16c57b1abb97e9538de2ab76f28a86c9e
AlonaRahilevych <ragilevic@gmail.com>
2024-09-11 13:09:16 +0200

    MainBlockComponent was updated

--- src/components/MainBlockComponent/MainBlockComponent.tsx
+++ src/components/MainBlockComponent/MainBlockComponent.tsx
@@ -1,7 +1,7 @@
 import { FC, useContext } from 'react';
 import MainContentComponent from '../MainContentComponent/MainContentComponent';
 import './MainBlockComponent.scss';
-import { ImageType } from '../../assets/types/ImageType';
+import { ImageType } from '../../types/ImageType';
 import { Pagination, Typography } from '@mui/material';
 import { ImagesContext } from '../../context/ImagesContext';
 import SearchComponent from '../SearchComponent/SearchComponent';
@@ -15,38 +15,53 @@ import SearchComponent from '../SearchComponent/SearchComponent';
 // };
 
 const MainBlockComponent = () => {
-  const { currentPage, amountOfPages, handlePageChange } =
+  const { currentPage, amountOfPages, setCurrentPage, error } =
     useContext(ImagesContext);
+
+  const handlePageChange = (
+    e: React.ChangeEvent<unknown>,
+    value: number
+  ): void => {
+    setCurrentPage(value);
+  };
   return (
     <main className='main'>
-      {/* <SearchBlock input={input} /> */}
       <div className='container main__container'>
         <SearchComponent />
-        <MainContentComponent /*images={images} */ />
 
-        <div className='pagination'>
-          <Pagination
-            count={amountOfPages}
-            showFirstButton
-            showLastButton
-            onChange={handlePageChange}
-            page={currentPage}
-            // sx={{
-            //   '& .MuiPaginationItem-root': {
-            //     color: 'yellow',
-            //     backgroundColor: 'black',
-            //   },
-            //   '& .MuiPaginationItem-root:hover': {
-            //     color: 'red',
-            //     backgroundColor: 'blue',
-            //   },
-            //   '& .Mui-selected': {
-            //     color: 'red',
-            //     backgroundColor: 'red',
-            //   },
-            // }}
-          />
-        </div>
+        {!error ? (
+          <>
+            <MainContentComponent />
+            <div className='pagination'>
+              <Pagination
+                count={amountOfPages}
+                showFirstButton
+                showLastButton
+                onChange={handlePageChange}
+                page={currentPage}
+                sx={{
+                  '& .MuiPaginationItem-root': {
+                    color: 'white',
+                    backgroundColor: 'rgb(17, 27, 67)',
+                  },
+                  '& .MuiPaginationItem-root:hover': {
+                    color: 'white',
+                    backgroundColor: '#778bdc',
+                  },
+                  '& .Mui-selected': {
+                    color: 'white',
+                    backgroundColor: '#1b2b6c',
+                  },
+                }}
+              />
+            </div>
+          </>
+        ) : (
+          <div className='main__error'>
+            <p>{error}</p>
+            <i className='ph ph-smiley-sad'></i>
+          </div>
+        )}
       </div>
     </main>
   );
