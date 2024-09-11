f267f0d25a19a1f20a6d1d63021bf509544d53a5
AlonaRahilevych <ragilevic@gmail.com>
2024-09-11 14:47:20 +0200

    footer was changed

--- src/components/FooterComponent/Footer.tsx
+++ src/components/FooterComponent/Footer.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Footers.scss';
 
-type FooterProps = {};
-
-const Footer = (props: FooterProps) => {
+const Footer = () => {
   return (
     <div className='footer'>
-      Footer
-      <a href='https://www.vecteezy.com/free-png/404'>404 PNGs by Vecteezy</a>
+      <div className='footer__social'>
+        <div className='footer__icon'>
+          <i className='ph ph-instagram-logo'></i>
+        </div>
+        <div className='footer__icon'>
+          <i className='ph ph-facebook-logo'></i>
+        </div>
+        <div className='footer__icon'>
+          <i className='ph ph-pinterest-logo'></i>
+        </div>
+        <div className='footer__icon'>
+          <i className='ph ph-twitter-logo'></i>
+        </div>
+      </div>
+      {/* <a href='https://www.vecteezy.com/free-png/404'>404 PNGs by Vecteezy</a> */}
     </div>
   );
 };
