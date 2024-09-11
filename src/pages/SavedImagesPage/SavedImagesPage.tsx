f93ce74cd249593a0b01fe5da5c66dce6829d3d1
AlonaRahilevych <ragilevic@gmail.com>
2024-09-11 13:13:42 +0200

    SavedImagesPage was updated

--- src/pages/SavedImagesPage/SavedImagesPage.tsx
+++ src/pages/SavedImagesPage/SavedImagesPage.tsx
@@ -1,62 +1,16 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { AuthContext } from '../../context/AutorizationContext';
-import { auth, db } from '../../config/firebase';
-import { collection, onSnapshot, query } from 'firebase/firestore';
 import './SavedImagesPage.scss';
-import { useParams } from 'react-router';
+import { Navigate, useParams } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { SaveImgContext } from '../../context/SaveImgContex';
 const SavedImagesPage = () => {
-  const { fetchSavedImg, images, deleteToFavorites, isLoggedIn } =
-    useContext(AuthContext);
-  console.log(auth.currentUser);
-  //   const img = images?.find((image) => image.url === url);
-
-  type User = {
-    email: string;
-    imagesList: Image[];
-  };
-  type Image = {
-    url: string;
-  };
-  //const [images, setImages] = useState<User[] | null>(null);
-
-  //   const getImagesRealTime = () => {
-  //     const q = query(collection(db, 'users'));
-  //     onSnapshot(q, (querySnapshot) => {
-  //       const usersArray: User[] = [];
-  //       querySnapshot.forEach((doc) => {
-  //         const imageWithId: User = {
-  //           email: doc.data().email,
-  //           imagesList: doc.data().imagesList,
-  //         };
-  //         //   messagesArray.push(doc.data().name);
-  //         usersArray.push(imageWithId);
-  //       });
-  //       setImages(usersArray);
-  //     });
-  //   };
-
-  //   const handleDeleteFromFavorites = () => {
-  //     console.log(auth.currentUser?.uid);
-  //     if (img && isLoggedIn) {
-  //       deleteToFavorites({ url: img?.largeImageURL })
-  //         .then(() => {
-  //           console.log('Image added to favorites');
-  //           alert('added');
-  //         })
-  //         .catch((error) => {
-  //           console.error('Error adding image to favorites:', error);
-  //           alert('error');
-  //         });
-  //     } else {
-  //       console.log('User must be logged in to add to favorites');
-  //     }
-  //   };
-
-  console.log(images);
+  const { fetchSavedImg, images, deleteFromSaved } = useContext(SaveImgContext);
   useEffect(() => {
     fetchSavedImg();
   }, []);
-
+  if (!images) {
+    return <Navigate to={'/'}></Navigate>;
+  }
   return (
     <div className='wrapper saved__wrapper'>
       <div className='container saved__container'>
@@ -65,16 +19,31 @@ const SavedImagesPage = () => {
             {images &&
               images.map((image, index) => (
                 <div className='saved__block'>
-                  <div className='image'>
-                    <img src={image.url} key={index} alt='' />
-                  </div>
-                  <button
-                    onClick={() => {
-                      deleteToFavorites(image);
-                      fetchSavedImg();
-                    }}>
-                    delete img
-                  </button>
+                  <NavLink to={`/image/${image.id}`}>
+                    <div className='image'>
+                      <img src={image?.url} alt='' />
+                      <div className='image__dark'>
+                        <div className='image__icons'>
+                          <div className='image__icon'>
+                            <i className='ph ph-chat-circle'></i>
+                          </div>
+                          <div className='image__icon'>
+                            <i
+                              className='ph ph-trash'
+                              onClick={(
+                                e: React.MouseEvent<HTMLDivElement>
+                              ) => {
+                                e.stopPropagation();
+                                e.preventDefault();
+                                deleteFromSaved(image);
+                                fetchSavedImg();
+                              }}></i>
+                          </div>
+                        </div>
+                        <div className='image__tags'>{image.tags}</div>
+                      </div>
+                    </div>
+                  </NavLink>
                 </div>
               ))}
           </div>
