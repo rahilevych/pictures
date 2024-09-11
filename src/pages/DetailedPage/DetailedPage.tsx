f7975c7005e9597a44f59cd88c79431671ba709f
AlonaRahilevych <ragilevic@gmail.com>
2024-09-11 13:07:18 +0200

    DetailedPage was updated

--- src/pages/DetailedPage/DetailedPage.tsx
+++ src/pages/DetailedPage/DetailedPage.tsx
@@ -1,87 +1,63 @@
-import React, { FC, useContext, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { ImageType } from '../../assets/types/ImageType';
+import React, { FC, useContext, useEffect } from 'react';
+import { NavLink, Navigate, useParams } from 'react-router-dom';
 import './DetailedPage.scss';
 import { AuthContext } from '../../context/AutorizationContext';
 import { auth } from '../../config/firebase';
+import CommentComponent from '../../components/CommentComponent/CommentComponent';
+import MessageComponent from '../../components/MessageComponent/MessageComponent';
+import { ImagesContext } from '../../context/ImagesContext';
+import { CommentsContext } from '../../context/CommentsContext';
+import { SaveImgContext } from '../../context/SaveImgContex';
+import { CommentType } from '../../types/CommentType';
 
-type DetailedPageProps = {
-  images: ImageType[] | null;
-};
-
-const DetailedPage: FC<DetailedPageProps> = ({ images }) => {
+const DetailedPage = () => {
   const { id } = useParams();
+  const { images } = useContext(ImagesContext);
   const img = images?.find((image) => image.id === Number(id));
-
-  const { addToFavorites, isLoggedIn } = useContext(AuthContext);
+  const { comments, fetchComments } = useContext(CommentsContext);
+  const { isLoggedIn } = useContext(AuthContext);
+  const { addToSaved } = useContext(SaveImgContext);
 
   const handleAddToFavorites = () => {
     console.log(auth.currentUser?.uid);
     if (img && isLoggedIn) {
-      addToFavorites({ url: img?.largeImageURL })
+      addToSaved({
+        url: img?.largeImageURL,
+        tags: img.tags,
+        id: img.id.toString(),
+      })
         .then(() => {
           console.log('Image added to favorites');
-          alert('added');
         })
         .catch((error) => {
           console.error('Error adding image to favorites:', error);
-          alert('error');
         });
     } else {
       console.log('User must be logged in to add to favorites');
     }
   };
   if (!img) {
-    return <div>Image not found</div>;
+    return <Navigate to={'/'}></Navigate>;
   }
+
+  useEffect(() => {
+    fetchComments(img.id);
+  }, []);
 
   return (
     <div className='wrapper details__wrapper'>
       <div className='container details__container'>
         <main className='details'>
-          <div className='details__descr'>
-            <div className='details__img'>
-              <img src={img?.webformatURL} alt='' />
-            </div>
-            <div className='details__info'>
-              <div className='details__row'>
-                <div className='details__icon'>
-                  <i className='ph ph-heart-straight'></i>
-                </div>
-                <div className='details__icon'>
-                  <i
-                    onClick={handleAddToFavorites}
-                    className='ph ph-bookmark-simple'></i>
-                </div>
-                <div className='details__icon'>
-                  <i className='ph ph-share-network'></i>
-                </div>
-              </div>
-              <div className='details__row'>
-                <p>Views</p>
-                <p>{img?.views}</p>
-              </div>
-              <div className='details__row'>
-                <p>Downloads</p>
-                <p>{img?.downloads}</p>
-              </div>
-              <div className='details__row'>
-                <p>Likes</p>
-                <p>{img?.likes}</p>
-              </div>
-              <div className='details__row'>
-                <p>Comments</p>
-                <p>{img?.comments}</p>
-              </div>
-              <div className='details__row'>
-                <div className='details__user-img'>
-                  <img src={img?.userImageURL} alt='' />
-                </div>
-                <p className='details__user'>{img?.user}</p>
-              </div>
-            </div>
+          <div className='details__img'>
+            <img src={img?.webformatURL} alt='' />
           </div>
-          <div className='details__coments'></div>
+          <div className='details__comments'>
+            <CommentComponent />
+            {comments &&
+              comments.map((comment: CommentType, index: number) => (
+                <MessageComponent key={index} comment={comment} />
+              ))}
+          </div>
         </main>
       </div>
     </div>
